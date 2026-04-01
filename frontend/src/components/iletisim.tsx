230b8817794d8b5e6a75c00d1725bf965cc740c6
Mete610 <119365508+Mete610@users.noreply.github.com>
2026-04-01 23:04:19 +0300

    Update iletisim.tsx

--- frontend/src/components/iletisim.tsx
+++ frontend/src/components/iletisim.tsx
@@ -32,7 +32,7 @@ function Iletisim() {
 
                     <div className="info-item">
                         <span className="info-icon">📞</span>
-                        <p><strong>Telefon:</strong> <a href="tel:+905337144046"><i class="fas fa-phone"></i> +90 5XX XXX XX XX</a></p>
+                        <p><strong>Telefon:</strong> <a href="tel:+905337144046"><i class="fas fa-phone"></i> +90 533 714 40 46</a></p>
 
                     </div>
                     <div className="info-item">
